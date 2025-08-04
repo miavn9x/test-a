ecbaafce1e651f91ef2f5ba3885433114a3b2f1d
Mia 9X <miavn9x@gmail.com>
2025-08-04 15:08:58 +0700

    1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -29,7 +29,11 @@ async function bootstrap() {
   // Bật CORS để cho phép frontend từ origin được chỉ định truy cập tài nguyên của backend
   app.enableCors({
     // origin: ['http://localhost:3232'],
-    origin: ['https://test-bsim.vercel.app', 'https://test-bsim-git-main-mia-9xs-projects.vercel.app','https://test-bsim-67l0jz6ak-mia-9xs-projects.vercel.app'],
+    origin: [
+      'https://test-bsim.vercel.app',
+      'https://test-bsim-git-main-mia-9xs-projects.vercel.app',
+      'https://test-bsim-67l0jz6ak-mia-9xs-projects.vercel.app',
+    ],
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
